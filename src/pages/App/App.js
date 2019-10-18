757fc2c1c2204def914db3a34c5217515d29cd8f
Daniel Camarena <dcamarena0229@gmail.com>
2019-10-18 16:34:36 -0700

    Refactoring directory structure, installing dependencies

---
+++ src/pages/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (<h1>Hi from home</h1>  );
+  }
+  async componentDidMount(){
+    
+  }
+};
+
+
+ 
+export default App;
