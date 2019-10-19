ea86c9532acb6f91251b8c8b0b43d5d83b52078e
Daniel Camarena <dcamarena0229@gmail.com>
2019-10-18 22:24:42 -0700

    creating a test component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
+import Test from '../Test';
 
 class App extends Component {
   constructor() {
@@ -19,12 +20,11 @@ class App extends Component {
                 this.state.user && this.state.role === 'admin' ? (
                   <React.Fragment>
                     <NavBar {...props} />
-                    <div>Ok awesome</div>
                   </React.Fragment>
                 ) : this.state.user && this.state.role === 'manager' ? (
                   <React.Fragment>
                     <h1>Manager View</h1>
-                    </React.Fragment>
+                  </React.Fragment>
                 ) : this.state.user && this.state.role === 'tech' ? (
                   <React.Fragment>
                     <h1>tech view</h1>
---
+++ src/pages/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = () => {
+    return ( <>RBAC TEST</> );
+}
+ 
+export default Test;
