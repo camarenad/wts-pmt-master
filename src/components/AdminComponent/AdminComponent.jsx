6410f2af88f061dfd4c9f3118b94c7604555129e
Daniel Camarena <dcamarena0229@gmail.com>
2019-10-20 22:34:10 -0700

    adding card functionality to admin page, connecting admin page to firestore, styling admin page and related components

--- src/components/AdminComponent/AdminComponent.jsx
+++ src/components/AdminComponent/AdminComponent.jsx
@@ -1,8 +1,62 @@
 import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+// import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 class AdminComponent extends Component {
   render() {
-    return <h1></h1>;
+    return (
+      <div>
+        {this.props.reports
+          ? this.props.reports.map(function(item, index) {
+              return (
+                <Card key={index}>
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      alt='Work Image'
+                      height='140'
+                      src={item.photoRefs.toString()}
+                      title='Work Image'
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant='h5' component='h2'>
+                        {item.bidZone}
+                      </Typography>
+                      <br></br>
+                      <Typography gutterBottom variant='subtitle2' component='h4'>
+                        ID: {item.jobId}
+                      </Typography>
+                      <br></br>
+                      <Typography
+                        gutterBottom
+                        variant='subtitle2'
+                        component='h3'
+                      >
+                        {item.jobLocation}
+                      </Typography>
+
+                      <br></br>
+                      <Typography
+                        variant='body2'
+                        color='textSecondary'
+                        component='p'
+                      >
+                        Remarks: <br></br>
+                        {item.jobSummary}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  {/* <CardActions></CardActions> */}
+                </Card>
+              );
+            })
+          : ''}
+      </div>
+    );
   }
 }
 
--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { Grid, Button, Box, Divider } from '@material-ui/core';
+import { Grid, Button, Divider } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import AdminComponent from '../../components/AdminComponent/AdminComponent';
 import FilterComponent from '../../components/FilterComponent/FilterComponent';
+import firebase from '../../firebase/firebase';
+
+const reportList = [];
 
 class AdminPage extends Component {
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.state = { id: '', address: '', reports: null };
   }
   handleSubmit = e => {
     console.log('sweet');
@@ -17,12 +21,24 @@ class AdminPage extends Component {
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  state = {};
+  componentDidMount = async () => {
+    const self = this;
+    const db = firebase.firestore();
+    const reports = db.collection('job-completion-report');
+    reports.get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        reportList.push(doc.data());
+        self.setState({
+          reports: reportList
+        });
+      });
+    });
+  };
   render() {
     return (
       <Container>
         <h1 style={{ textAlign: 'center', marginBottom: 25, marginTop: 25 }}>
-          Filter Job Completion Reports
+          Job Completion Reports
         </h1>
         <Divider style={{ marginBottom: 20 }} />
         <Grid item xs={12}>
@@ -37,18 +53,20 @@ class AdminPage extends Component {
             name='address'
           />
           <form onSubmit={this.handleSubmit}>
-            <AdminComponent />
+            <div style={{ textAlign: 'center' }}>
+              <Button
+                variant='contained'
+                margin='normal'
+                color='primary'
+                type='submit'
+              >
+                Submit
+              </Button>
+            </div>
           </form>
-          <div style={{ textAlign: 'center' }}>
-            <Button
-              variant='contained'
-              margin='normal'
-              color='primary'
-              type='submit'
-            >
-              Submit
-            </Button>
-          </div>
+          <Grid item xs={12} style={{marginTop:50}}>
+          <AdminComponent reports={this.state.reports} />
+          </Grid>
         </Grid>
       </Container>
     );
